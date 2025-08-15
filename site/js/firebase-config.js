d26219f13f430567347b090f004d0469477dc764
MollaMick <153179649+mmgrafisk@users.noreply.github.com>
2025-08-15 14:03:09 +0200

    Add files via upload

--- site/js/firebase-config.js
+++ site/js/firebase-config.js
@@ -1,9 +1,3 @@
-
-window._FIREBASE_CONFIG = {
-  apiKey: "AIzaSyA_LQ2KpSII31mR9mbeUMBzpx9JYy35T2g",
-  authDomain: "mit-kontrolcenter.firebaseapp.com",
-  projectId: "mit-kontrolcenter",
-  storageBucket: "mit-kontrolcenter.appspot.com",
-  messagingSenderId: "981412245600",
-  appId: "1:981412245600:web:5fbde74608b29fd59bd305"
-};
+// Denne fil genereres automatisk af build-scriptet på Netlify.
+// Efterlad den tom i dit repository.
+window._FIREBASE_CONFIG = {};
